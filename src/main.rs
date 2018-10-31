95b3e8ec7cd0427acedade1148167ddb6861159a
Roshan Giyanani <roshangiyanani@gmail.com>
2018-10-30 23:00:11 -0400

    Convert to run without the standard library and a host os.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// default start function (for macOS)
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
 }
