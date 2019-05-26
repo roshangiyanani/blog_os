12d0cf9873da6c56ba10b017cf96adf6b1545ab9
Roshan Giyanani <roshangiyanani@gmail.com>
2019-05-25 23:52:44 -0400

    add basic_boot and panic_handler integration tests

---
+++ tests/panic_handler.rs
@@ -0,0 +1,69 @@
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+use core::fmt;
+use core::fmt::Write;
+use core::panic::PanicInfo;
+
+use blog_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
+
+const MESSAGE: &str = "Example panic message from panic_handler test";
+const PANIC_LINE: u32 = 17; // adjust this when moving the `panic!` call
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    serial_print!("panic_handler...");
+    panic!(MESSAGE); // must be in line `PANIC_LINE`
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    check_message(info);
+    check_location(info);
+
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+fn check_message(info: &PanicInfo) {
+    let message = info.message().unwrap_or_else(|| fail("no message"));
+    let mut compare_message = CompareMessage { expected: MESSAGE };
+    write!(&mut compare_message, "{}", message).unwrap_or_else(|_| fail("write failed"));
+    if !compare_message.expected.is_empty() {
+        fail("message shorter than expected message");
+    }
+}
+
+fn check_location(info: &PanicInfo) {
+    let location = info.location().unwrap_or_else(|| fail("no location"));
+    if location.file() != file!() {
+        fail("file name wrong");
+    }
+    if location.line() != PANIC_LINE {
+        fail("file line wrong");
+    }
+}
+
+fn fail(error: &str) -> ! {
+    serial_println!("[failed]");
+    serial_println!("{}", error);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+struct CompareMessage {
+    expected: &'static str,
+}
+
+impl fmt::Write for CompareMessage {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        if self.expected.starts_with(s) {
+            self.expected = &self.expected[s.len()..];
+        } else {
+            fail("message not equal to expected message");
+        }
+        Ok(())
+    }
+}
